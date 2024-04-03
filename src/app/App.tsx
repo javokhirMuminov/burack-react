6417e10786e5421d47e3e18c9b7a7da07c2bac14
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-04-03 12:29:49 +0900

    fix: develop burack  screen based components

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,52 +1,35 @@
 import React from 'react';
 import '../css/app.css';
 import { Box, Container, Stack } from '@mui/system';
-import { Link, Switch, Typography } from '@mui/material';
+import { Link,  Typography } from '@mui/material';
 import { Button } from '@mui/base';
 import { ProductsPage } from './screens/productsPage';
 import { OrdersPage } from './screens/ordersPage';
 import { UserPage } from './screens/userPage';
-import { Router } from 'react-router';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { HomePage } from './screens/homePage';
+
 
 
 function App() {
   return (
-    <div>
+  <>
 
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">HomePage</Link>
-          </li>
-          <li>
-            <Link to="/products">ProductsPage</Link>
-          </li>
-          <li>
-            <Link to="/orders">OrdersPage</Link>
-          </li>
-          <li>
-            <Link to="/member-page">UserPage</Link>
-          </li>
-          <li>
-            <Link to="/helpPage">HelpPage</Link>
-          </li>
-        </ul>
-      </nav>
-
-
-
-       <Switch>
-      <Router path="/products">
-          <ProductsPage />
-      </Router>
-      <Router path="/orders">
-          <OrdersPage />
-      </Router>
-      <Router path="/member-page">
-          <UserPage />
-      </Router>
+    <Switch>
+      <Route path="/products">
+        <ProductsPage />
+      </Route>
+      <Route path="/orders">
+        <OrdersPage />
+      </Route>
+      <Route path="/member-page">
+        <UserPage />
+      </Route>
+      <Route path="/">
+        <HomePage />
+      </Route>
     </Switch>
-    </div>
+    </>
 
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router  } from 'react-router-dom';
 import './css/index.css';
 import theme from './app/MaterialTheme';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
 
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
@@ -23,7 +22,9 @@ root.render(
         </Router>
       </ThemeProvider>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+
+document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
