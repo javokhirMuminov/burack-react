d298497b0177512de667e3a0ff48f0e5fdc4b482
javokhirMuminov <javokhirmuminovb@gmail.com>
2024-05-04 21:10:25 +0900

    feat: develop member update business logic

--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -15,6 +15,7 @@ import { Order, OrderInquiry } from "../../../lib/types/order";
 import { OrderStatus } from "../../../lib/enums/order.enum";
 import OrderService from "../../services/OrderService";
 import { useGlobals } from "../../hooks/useGlobals";
+import { useHistory } from "react-router-dom";
 
 
 
@@ -33,7 +34,8 @@ export default function OrdersPage() {
   { setProcessOrders, setPausedOrders, setFinishedOrders } =
     actionDispatch(useDispatch());
 
-  const {orderBuilder} =  useGlobals();
+  const {orderBuilder, authMember} =  useGlobals();
+  const history = useHistory();
   const [value, setValue] = useState("1");
   const [orderInquiry, setOrderInquiry] = useState<OrderInquiry>({
     page: 1,
@@ -62,6 +64,8 @@ export default function OrdersPage() {
       .catch((err) => console.log(err));
   }, [orderInquiry, orderBuilder]);
   /**H**/
+
+  if(!authMember) history.push("/");
 
   return (
     <div className="order-page">
